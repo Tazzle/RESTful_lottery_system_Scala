e7bcf0cd9a6f4a864c1e4f87b3ae77699a52cd7a
Tazzle <tazzle@tazzle.com>
2018-01-21 14:49:55 +0000

    add ticket, possible sort line method required

--- src/main/scala/com/api/models/Ticket.scala
+++ src/main/scala/com/api/models/Ticket.scala
@@ -1,5 +1,11 @@
 package com.api.models
 
-class Ticket {
+class Ticket (Id : String, Lines : Array[Line]){
+
+  private var _id = Id
+  private var _lines = Lines
+
+  def id = _id
+  def lines = _lines
 
 }
